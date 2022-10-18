b2268df3198e8b4fb18dd6513ae1c3420cff56cb
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-10-18 09:11:34 -0500

    basic setup done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "./Navbar"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
 const Main = () => {
   return <div id="main">
     <Navbar />
-    <h1> I am main </h1>
+    <h1> Today's hot items </h1>
   </div>;
 };
 
