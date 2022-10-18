e4825e242b39108f23c8971b77461c3edbdb5b03
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-10-18 09:28:51 -0500

    committed atrocities during start-up but got it to work. Will begin working on the navbar component and a few default CSS.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {NavLink} from "react-router-dom";
+
 
 const Navbar = () => {
   return <div id="Navbar">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from "react-dom/client"
-import {Main} from './components'
+import {Navbar} from "./components"
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+  } from 'react-router-dom';
+
+import {
+    getCurrentUser
+  } from './auth';
 
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
 
-root.render(<Main />)
+
+const App = () => {
+    const [currentUser, setCurrentUser] = useState(getCurrentUser()); 
+    return <div id="App">
+      <Navbar
+      currentUser={currentUser} />
+      <h1> Today's hot items </h1>
+    </div>;
+  };
+
+
+root.render(<App />)
